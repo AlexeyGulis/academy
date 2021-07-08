54aa5a5f120d07e051dabaf806abe1e530aae490
AlexeyGulis <kiri.dh@yandex.ru>
2021-07-08 23:54:18 +0300

    Home project

---
+++ src/by/academy/lesson1/AnimalDemo.java
@@ -0,0 +1,11 @@
+package by.academy.lesson1;
+
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal();
+        Animal animal2 = new Animal(5, "Frank");
+        System.out.println(animal1.getAge());
+        System.out.println(animal2.getAge());
+    }
+}
