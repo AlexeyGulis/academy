ad7ad935cf3e6135ebc68afde8bb3ddf6e352ee8
AlexeyGulis <kiri.dh@yandex.ru>
2021-07-08 23:39:05 +0300

    Home project

---
+++ src/by/itacademy/test/Animal.java
@@ -0,0 +1,24 @@
+package by.itacademy.test;
+
+public class Animal {
+    int age;
+    String name;
+    public Animal(){
+    }
+    public Animal(int age,String name){
+        this.name=name;
+        this.age=age;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+}
