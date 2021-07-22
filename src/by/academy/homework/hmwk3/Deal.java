8b93ead7c3ab42496b55999019a94d3e848f818f
AlexeyGulis <kiri.dh@yandex.ru>
2021-07-22 16:21:14 +0300

    homework3

--- src/by/academy/homework/hmwk3/Deal.java
+++ src/by/academy/homework/hmwk3/Deal.java
@@ -18,9 +18,9 @@ public class Deal {
             if("p1".equals(nameProduct[i])){
                 products[i] = new Type1(nameProduct[i],price[i],count[i]);
             }else if("p2".equals(nameProduct[i])){
-                products[i] = new Type1(nameProduct[i],price[i],count[i]);
+                products[i] = new Type2(nameProduct[i],price[i],count[i]);
             }else if("p3".equals(nameProduct[i])){
-                products[i] = new Type1(nameProduct[i],price[i],count[i]);
+                products[i] = new Type3(nameProduct[i],price[i],count[i]);
             }else products[i] = new Product(nameProduct[i],price[i],count[i]);
         }
         this.date = date;
