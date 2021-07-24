66dbce9b47537f5199fc6a7a24fd59554fee0333
AlexeyGulis <kiri.dh@yandex.ru>
2021-07-24 14:22:27 +0300

    homework2

--- src/by/academy/homework/hmwk2/Task1.java
+++ src/by/academy/homework/hmwk2/Task1.java
@@ -1,5 +1,6 @@
 package by.academy.homework.hmwk2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
@@ -14,6 +15,7 @@ public class Task1 {
         if (str1.length() != str2.length()) {
             System.out.println(flag);
         } else {
+            /* Первая версия
             char[] temp1 = str1.toCharArray();
             for (int i = 0; i < temp1.length; i++) {
                 if (str1.replace(Character.toString(temp1[i]), "").length() == str2.replace(Character.toString(temp1[i]), "").length()) {
@@ -23,6 +25,19 @@ public class Task1 {
                     break;
                 }
             }
+            Вторая версия
+            */
+            char[] chstr1 = str1.toCharArray();
+            char[] chstr2 = str2.toCharArray();
+            int[] eng1 = new int[128];
+            int[] eng2 = new int[128];
+            for (int i = 0; i < chstr1.length; i++) {
+                eng1[chstr1[i]]++;
+            }
+            for (int i = 0; i < chstr2.length; i++) {
+                eng2[chstr2[i]]++;
+            }
+            flag = Arrays.equals(eng1,eng2);
             System.out.println(flag);
         }
         scan.close();
