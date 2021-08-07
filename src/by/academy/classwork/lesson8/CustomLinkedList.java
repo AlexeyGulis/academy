c8840bb92e1faa115d4908c7ed093c8da9a51c11
AlexeyGulis <wadim.sidoryk@mail.ru>
2021-08-07 15:23:41 +0300

    lesson9

--- src/by/academy/classwork/lesson8/CustomLinkedList.java
+++ src/by/academy/classwork/lesson8/CustomLinkedList.java
@@ -1,37 +1,133 @@
 package by.academy.classwork.lesson8;
 
-public class CustomLinkedList<T> {
+import java.util.Iterator;
+
+public class CustomLinkedList<T> implements Iterable<T> {
 
     private Node<T> head;
     private Node<T> tail;
     private Integer size = 0;
 
-    public void add(T item){
+    public void add(T item) {
+
+        Node<T> node = new Node<>();
+        node.value = item;
+
+        if (tail == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    public void print() {
+        Node<T> value = head;
+        while (value != null) {
+            System.out.print(value.value + " ");
+            value = value.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        CustomLinkedList<Integer> list = new CustomLinkedList<>();
+        list.add(4);
+        list.add(55);
+        list.add(23);
+
+        list.print();
+
+        list.remove(2);
+        list.print();
+    }
+
+    public void remove(int index) {
+        if (size == 1 && index == 0) {
+            head = null;
+            tail = null;
+            size = 0;
+            return;
+        }
+        if (index == size - 1) {
+            Node<T> pref = tail.prev;
+            pref.next = null;
+            tail = pref;
+            size--;
+            return;
+        }
+        if (index >= 0 || index < size) {
+            System.out.println("remove");
+            int count = 0;
+            Node<T> value = head;
+            while (value != null) {
+                if (count == index) {
+                    break;
+                }
+                value = value.next;
+                count++;
+            }
+            Node<T> pref = value.prev;
+            Node<T> next = value.next;
+            if (index == 0) {
+                head = next;
+            }
+            if (pref != null) {
+                pref.next = next;
+            }
+            next.prev = pref;
+            size--;
+            return;
+        }
+        System.out.println("indexOutBound");
+    }
+
+    public void set(int index, T item) {
 
     }
 
-    public void remove(int index){
-
-    }
-
-    public void set(int index,T item){
-
-    }
-
-    public T get(int index){
+    public T get(int index) {
         return null;
     }
 
-    class Node<T>{
+    @Override
+    public InnerIterator<T> iterator() {
+        return new InnerIterator<>();
+    }
+
+    class InnerIterator<T> implements Iterator<T> {
+        Node<T> current = null;
+
+        @Override
+        public boolean hasNext() {
+            if (current == null) {
+                return head != null;
+            }
+            return current.next != null;
+        }
+
+        @Override
+        public T next() {
+            if (current == null) {
+                //current = head;
+            }
+            return current.next.value;
+        }
+    }
+
+    class Node<T> {
         Node<T> next;
-        Node<T> pref;
+        Node<T> prev;
         T value;
 
         @Override
         public String toString() {
-            final StringBuilder sb = new StringBuilder("Node{");
+            StringBuilder sb = new StringBuilder("Node{");
             sb.append("next=").append(next);
-            sb.append(", pref=").append(pref);
+            sb.append(", pref=").append(prev);
             sb.append(", value=").append(value);
             sb.append('}');
             return sb.toString();
---
+++ src/by/academy/classwork/lesson8/HeavyBox.java
@@ -0,0 +1,72 @@
+package by.academy.classwork.lesson8;
+
+import java.util.ArrayList;
+
+class HeavyBox {
+    int weight;
+    int height;
+    int depth;
+    int width;
+
+    HeavyBox(int w, int h, int d, int m) {
+        width = w;
+        height = h;
+        depth = d;
+        weight = m;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("HeavyBox{");
+        sb.append("weight=").append(weight);
+        sb.append(", height=").append(height);
+        sb.append(", depth=").append(depth);
+        sb.append(", width=").append(width);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<HeavyBox> list = new ArrayList<>();
+        list.add(new HeavyBox(1,2,3,4));
+        list.add(new HeavyBox(2,3,3,4));
+        list.add(new HeavyBox(3,5,3,5));
+        list.add(new HeavyBox(1,2,1,1));
+        list.forEach((n) -> System.out.println(n));
+        list.get(0).setWeight(1);
+        list.remove(list.get(list.size()-1));
+
+    }
+}
