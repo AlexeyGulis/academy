ade1b70c96545dd67df53de4e7ed3f5980a7054e
AlexeyGulis <kiri.dh@yandex.ru>
2021-08-12 17:17:38 +0300

    hmwk5

--- src/by/academy/homework/hmwk5/task5/Task5.java
+++ src/by/academy/homework/hmwk5/task5/Task5.java
@@ -1,10 +1,22 @@
 package by.academy.homework.hmwk5.task5;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Task5 {
     public static void main(String[] args) {
-        Map<Character,Integer> freqChar;
-
+        String text = "I like java!!";
+        char[] chars = text.toCharArray();
+        int[] freq = new int[128];
+        for (int i = 0; i < chars.length; i++) {
+            freq[chars[i]]++;
+        }
+        Map<Character, Integer> freqChar = new HashMap<>();
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                freqChar.put((char) i, freq[i]);
+            }
+        }
+        freqChar.forEach((k, v) -> System.out.println(k + " в количестве " + v));
     }
 }
