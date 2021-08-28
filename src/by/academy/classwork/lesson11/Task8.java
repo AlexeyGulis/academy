66c054dc9879638d9d60166e549214e4ded460e7
AlexeyGulis <kiri.dh@yandex.ru>
2021-08-28 13:18:31 +0300

    lesson11

--- src/by/academy/classwork/lesson11/Task8.java
+++ src/by/academy/classwork/lesson11/Task8.java
@@ -6,9 +6,7 @@ import java.util.function.Supplier;
 public class Task8 {
     public static void main(String[] args) {
         Random r = new Random();
-        Supplier<Integer> te = () -> {
-            return r.nextInt(10);
-        };
+        Supplier<Integer> te = () -> r.nextInt(10);
         System.out.println(te.get());
     }
 }
--- src/by/academy/classwork/lesson11/Task9.java
+++ src/by/academy/classwork/lesson11/Task9.java
@@ -1,1 +1,11 @@
+package by.academy.classwork.lesson11;
 
+public class Task9{
+    static void print(){
+        System.out.println("Something");
+    }
+    public static void main(String[] args) {
+        Printable pr = Task9::print;
+        pr.print();
+    }
+}
